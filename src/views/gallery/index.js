9e76a17d8f28a0a59e326cf8a248ce1ae6fbb9af
mfathony115 <mfathony115@gmail.com>
2024-01-06 16:33:29 +0700

    add context in galeri and home

--- src/views/gallery/index.js
+++ src/views/gallery/index.js
@@ -1,7 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Banner from "../../components/Banner"
 import Cardimage from "../../components/Cardimage"
+import { useTextContext } from "../../context/TextContext"
+
 const Component = () => {
+    const { setShowText } = useTextContext()
+    useEffect(() => {
+        setShowText(true)
+    },[setShowText])
     return(
         <>
             <main>
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Banner from "../../components/Banner"
 import Download from "../../components/Download"
 import Carousel from "../../components/Carousel"
 import Cardimage from "../../components/Cardimage"
+import { useTextContext } from "../../context/TextContext"
+
 const Component = () => {
+    const { setShowText } = useTextContext()
+    useEffect(() => {
+        setShowText(true)
+    },[setShowText])
     return (
         <>
         <main>
